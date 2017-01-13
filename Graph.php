b14a29a803a0c2a323f884085ef90599a99866cd
pb-nurszultan <pal.balazs2@gmail.com>
2017-01-13 19:57:58 +0100

    fejl.

--- Graph.php
+++ Graph.php
@@ -16,21 +16,63 @@
 		}
 		
 		public function addNode(Node $node) {
+			$name = $node->getName();
+			if (in_array($name, array_keys($this->nodes))) {
+				throw new Exception('node_already_exists: '.$name);	
+			} else {
+				$this->nodes[$name] = $node;	
+			}
 		}
 		
 		public function getNode($name) {
+			$node = $this->nodes[$name];
+			if (is_null($node)) {
+				throw new Exception('node_not_exists: '.$name);
+			} else {
+				return $node;	
+			}
 		}
 		
 		public function addEdge(Edge $edge) {
+			$name = $edge->getName();
+			if (in_array($name, array_keys($this->edges))) {
+				throw new Exception('edge_already_exists: '.$name);	
+			} else {
+				$sourceNode = $edge->getSourceNode();
+				$targetNode = $edge->getTargetNode();
+				if ($this->isNodeExists($sourceNode) === false || $this->isNodeExists($targetNode) === false) {
+					throw new Exception('source_or_target_node_doest_exist: '.$sourceNode.', '.$targetNode);	
+				} else {
+					$this->edges[$name] = $edge;
+					$this->getNode($sourceNode)->addToSources($targetNode);
+					$this->getNode($targetNode)->addToTargets($sourceNode);
+				}
+			}
 		}
 		
 		public function getEdge($sNodeName, $tNodeName) {
+			$name = $sNodeName.'&&'.$tNodeName;
+			$edge = $this->edges[$name];
+			if (is_null($edge)) {
+				throw new Exception('edge_not_exists: '.$name);
+			} else {
+				return $edge;	
+			}
 		}
 		
 		public function isNodeExists($name) {
+			if (in_array($name, array_keys($this->nodes))) {
+				return true;	
+			}
+			return false;
 		}
 		
 		public function isEdgeExists($sNodeName, $tNodeName) {
+			$name = $sNodeName.'&&'.$tNodeName;
+			if (in_array($name, array_keys($this->edges))) {
+				return true;	
+			}
+			return false;
 		}
 		
 		public static function createGraph($type = null) {
@@ -54,6 +96,26 @@
 			}
 		}
 		
+		public function getName() {
+			return $this->name;	
+		}
+		
+		public function addToSources($edgeName) {
+			array_push($this->edgesSourceOf, $edgeName);
+		}
+		
+		public function addToTargets($edgeName) {
+			array_push($this->edgesTargetOf, $edgeName);
+		}
+		
+		public function getSources() {
+			return $this->edgesTargetOf;	
+		}
+		
+		public function getTargets() {
+			return $this->edgesSourceOf;	
+		}
+		
 		public static function createNode($name, $initdata=null) {
 			$node = new Node($name, $initdata);
 			return $node;
@@ -75,6 +137,18 @@
 			}
 			$this->sourceNode = $sNodeName;
 			$this->targetNode = $tNodeName;
+		}
+		
+		public function getSourceNode() {
+			return $this->sourceNode;	
+		}
+		
+		public function getTargetNode() {
+			return $this->targetNode;	
+		}
+		
+		public function getName() {
+			return $this->name;	
 		}
 		
 		public static function createEdge($sNodeName, $tNodeName, $initdata=null) {
--- graphtest.php
+++ graphtest.php
@@ -10,9 +10,41 @@
 	$g->addNode(Node::createNode('NÁNDOR'));
 	$g->addNode(Node::createNode('ANDRÁS'));
 	$g->addNode(Node::createNode('BÉLA'));
+	//$g->addNode(Node::createNode('BÉLA')); /* testing duplication error */
 
 	$g->addEdge(Edge::createEdge('ANDRÁS', 'BÉLA', array('weight'=>8)));
 	$g->addEdge(Edge::createEdge('NÁNDOR', 'BÉLA', array('weight'=>2)));
 	$g->addEdge(Edge::createEdge('ANDRÁS', 'KÁLMÁN', array('weight'=>15)));
 	$g->addEdge(Edge::createEdge('KÁLMÁN', 'NÁNDOR', array('weight'=>30)));
 	$g->addEdge(Edge::createEdge('ÁBEL', 'KÁLMÁN', array('weight'=>7)));
+	//$g->addEdge(Edge::createEdge('ÁBEL', 'KÁLMÁN', array('weight'=>7))); /* testing duplication error */
+	//$g->addEdge(Edge::createEdge('ÁBEL', 'BALÁZS', array('weight'=>80))); /* testing for not existing node */
+
+	echo "<hr />";
+
+	var_dump($g);
+
+	echo "<hr />";
+
+	var_dump($g->isNodeExists('BÉLA')); // should be TRUE
+	var_dump($g->isNodeExists('KÁLMÁN')); // should be TRUE
+	var_dump($g->isNodeExists('BALÁZS')); // should be FALSE
+
+	echo "<hr />";
+
+	var_dump($g->isEdgeExists('NÁNDOR', 'BÉLA')); // should be TRUE
+	var_dump($g->isEdgeExists('BÉLA', 'NÁNDOR')); // should be TRUE if UNDIRECTED GRAPH, should be FALSE if DIRECTED GRAPH
+	var_dump($g->isEdgeExists('KÁLMÁN', 'BÉLA')); // should be FALSE
+	var_dump($g->isEdgeExists('BALÁZS', 'ZOLTÁN')); // should be FALSE
+
+	echo "<hr />";
+
+	$n = $g->getNode('ANDRÁS');
+	var_dump($n->getSources()); // empty in DIRECTED GRAPH (there arent any edge to this node)
+	var_dump($n->getTargets()); // 'BÉLA' and 'KÁLMÁN' in DIRECTED GRAPH (there are edges from this node to BÉLA and KÁLMÁN)
+
+	echo "<hr />";
+
+	$n = $g->getNode('NÁNDOR');
+	var_dump($n->getSources());
+	var_dump($n->getTargets());
